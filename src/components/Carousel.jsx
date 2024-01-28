3993b8437d7a0abbce7fa479bbce3eef6b9f49c3
wooyoung0407 <wooyoung0407@naver.com>
2024-01-28 21:36:38 +0900

    조회완료

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,33 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import CarouselItem from "./CarouselItem";
 
-const mockItems = [
-  {
-    id: "1",
-    title: "카파도키아",
-    content:
-      "터키 카파도키아의 지명'아름다운 말이 있는 곳'이란 페르시아어에서 유래했다고 합니다. 명물로는 벌룬투어와 동굴교회 괴레메 야외박물관,지하도시 데린쿠유, 파샤바 계곡 등이 있습니다.",
-    country: "튀르키에공화국",
-  },
-  {
-    id: "2",
-    title: "한라산",
-    content: "제주도에 있는 아름다운 산.",
-    country: "대한민국(제주도)",
-  },
-  {
-    id: "3",
-    title: "교토",
-    content: "교토의 장소",
-    country: "일본",
-  },
-];
-
 export default function Carousel() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://diary-back.fly.dev/api/v1/diary");
+        const data = await response.json();
+        setItems(data.data);
+      } catch (error) {
+        console.error("데이터 가져오기 오류:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const settings = {
     dots: false,
@@ -40,9 +32,15 @@ export default function Carousel() {
   return (
     <div className="carousel-container w-3/5 h-full m-auto">
       <Slider {...settings}>
-        {items.map((item, i) => {
-          return <CarouselItem key={item.id} item={item} index={i} />;
-        })}
+        {items.map((item, i) => (
+          <CarouselItem
+            key={item.id}
+            title={item.title}
+            content={item.content}
+            country={item.country}
+            image={item.image}
+          />
+        ))}
       </Slider>
     </div>
   );
--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import { RiDeleteBinFill } from "react-icons/ri";
 import { LuPenSquare } from "react-icons/lu";
 import { Link } from "react-router-dom";
-import travel1 from "../assets/travel1.jpg";
-import travel2 from "../assets/travel2.jpg";
-import travel3 from "../assets/travel3.jpg";
 
-const covers = [travel1, travel2, travel3];
-
-export default function CarouselItem({ item, index }) {
+export default function CarouselItem({ title, country, image, content }) {
   return (
     <div className="w-full m-auto mt-10 carousel-item flex-col rounded-2xl border shadow-lg">
       <div className="w-full p-5 flex justify-between items-center border-b-2 border-purple-500">
@@ -25,16 +20,16 @@ export default function CarouselItem({ item, index }) {
         </div>
       </div>
       <div className="w-full h-full flex justify-center border-b-2 border-purple-500">
-        <img src={covers[index] } className="w-5/6 h-[500px] object-cover p-5"/>
+        <img src={image} alt={title} className="w-5/6 h-[500px] object-cover p-5" />
       </div>
       <div className="w-9/12 m-auto mt-4">
-        <span className="font-bold text-4xl">{item.title}</span>
+        <span className="font-bold text-4xl">{title}</span>
         <span className="ml-4 text-sm font-bold text-gray-500 underline">
-          {item.country}
+          {country}
         </span>
       </div>
       <div className="w-10/12 h-60 m-auto mt-4 p-5">
-        <span className="h-full text-xl font-bold">{item.content}</span>
+        <span className="h-full text-xl font-bold">{content}</span>
       </div>
     </div>
   );
